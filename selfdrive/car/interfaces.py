0b3d2fb279934025d5e6a294aa2f7e56c780e17f
Tim Wilson <twilsonco@gmail.com>
2023-09-01 19:13:39 -0600

    nnff: add friction override

--- selfdrive/car/interfaces.py
+++ selfdrive/car/interfaces.py
@@ -89,7 +89,9 @@ class FluxModel:
         activation = activation.replace(k, v)
       self.layers.append((W, b, activation))
     
+    
     self.validate_layers()
+    self.check_for_friction_override()
     
   # Begin activation functions.
   # These are called by name using the keys in the model json file
@@ -128,6 +130,10 @@ class FluxModel:
     for W, b, activation in self.layers:
       if not hasattr(self, activation):
         raise ValueError(f"Unknown activation: {activation}")
+  
+  def check_for_friction_override(self):
+    y = self.evaluate([10.0, 0.0, 0.2])
+    self.friction_override = (y < 0.1)
   
 def get_nn_model_path(car, eps_firmware) -> Tuple[Union[str, None, float]]:
   def check_nn_path(check_model):
