cb60be32039a6dc1cb5cc822e2563f8a14200407
Andrei Radulescu <andi.radulescu@gmail.com>
2023-11-23 11:02:50 +0200

    Changes to devcontainer to work with a OrbStack VM

--- .devcontainer/container_post_create.sh
+++ .devcontainer/container_post_create.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-TARGET_USER=batman
+TARGET_USER=andiradulescu
 source .devcontainer/.host/.env
 
 # override display flag for mac hosts
@@ -10,7 +10,7 @@ if [[ $HOST_OS == darwin ]]; then
 source .devcontainer/.host/.env
 if [ -n "\$HOST_DISPLAY" ]; then
   DISPLAY_NUM=\$(echo "\$HOST_DISPLAY" | awk -F: '{print \$NF}')
-  export DISPLAY=host.docker.internal:\$DISPLAY_NUM
+  export DISPLAY=host.orb.internal:\$DISPLAY_NUM
 fi
 EOF
 fi
@@ -21,7 +21,7 @@ if [[ $HOST_OS == darwin ]]; then
   cat <<EOF >> /home/$TARGET_USER/.bashrc
 if [ -n "\$HOST_DISPLAY" ]; then
   export VGL_PORT=10000
-  export VGL_CLIENT=host.docker.internal
+  export VGL_CLIENT=host.orb.internal
   export VGL_COMPRESS=rgb
   export VGL_DISPLAY=:99
   export VGL_FPS=60
@@ -33,6 +33,6 @@ fi
 
 # These lines are temporary, to remain backwards compatible with old devcontainers
 # that were running as root and therefore had their caches written as root
-sudo chown -R $TARGET_USER: /tmp/scons_cache
-sudo chown -R $TARGET_USER: /tmp/comma_download_cache
-sudo chown -R $TARGET_USER: /home/batman/.comma
+# sudo chown -R $TARGET_USER: /tmp/scons_cache
+# sudo chown -R $TARGET_USER: /tmp/comma_download_cache
+# sudo chown -R $TARGET_USER: /home/batman/.comma
