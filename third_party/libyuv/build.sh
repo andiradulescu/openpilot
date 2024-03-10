5e993f3f8e430220568ad8d40bb6a32fa10f0e0d
Andrei Radulescu <andi.radulescu@gmail.com>
2024-03-10 18:40:57 +0200

    refactored libyuv build.sh

--- third_party/libyuv/build.sh
+++ third_party/libyuv/build.sh
@@ -1,10 +1,26 @@
 #!/usr/bin/env bash
 set -e
 
-git clone https://chromium.googlesource.com/libyuv/libyuv
+DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null && pwd)"
+
+ARCHNAME=$(uname -m)
+if [ -f /TICI ]; then
+  ARCHNAME="larch64"
+fi
+
+cd $DIR
+if [ ! -d libyuv ]; then
+  git clone https://chromium.googlesource.com/libyuv/libyuv
+fi
+
 cd libyuv
 git reset --hard 4a14cb2e81235ecd656e799aecaaf139db8ce4a2
 cmake .
+make -j$(nproc)
+
+INSTALL_DIR="$DIR/$ARCHNAME"
+cp $DIR/libyuv/libyuv.a $INSTALL_DIR/lib
+cp -r $DIR/libyuv/include/* $DIR/include
 
 ## To create universal binary on Darwin:
 ## ```
