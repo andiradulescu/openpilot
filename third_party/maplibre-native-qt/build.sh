a12cdbec5547c8a15eab6a5acd424c83fb98d805
Andrei Radulescu <andi.radulescu@gmail.com>
2024-03-11 21:12:11 +0200

    updated maplibre build.sh

--- third_party/maplibre-native-qt/build.sh
+++ third_party/maplibre-native-qt/build.sh
@@ -3,7 +3,7 @@ set -e
 
 DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null && pwd)"
 
-ARCHNAME="x86_64"
+ARCHNAME=$(uname -m)
 MAPLIBRE_FLAGS="-DMLN_QT_WITH_LOCATION=OFF"
 if [ -f /AGNOS ]; then
   ARCHNAME="larch64"
@@ -12,13 +12,15 @@ fi
 
 cd $DIR
 if [ ! -d maplibre ]; then
-  git clone git@github.com:maplibre/maplibre-native-qt.git $DIR/maplibre
+  git clone --single-branch https://github.com/maplibre/maplibre-native-qt.git $DIR/maplibre
 fi
 
 cd maplibre
-git fetch --all
 git checkout 3726266e127c1f94ad64837c9dbe03d238255816
 git submodule update --depth=1 --recursive --init
+
+# requirement to build
+sudo apt install libqt5opengl5-dev
 
 # build
 mkdir -p build
@@ -28,11 +30,8 @@ cmake $MAPLIBRE_FLAGS $DIR/maplibre
 make -j$(nproc) || make -j2 || make -j1
 
 INSTALL_DIR="$DIR/$ARCHNAME"
-rm -rf $INSTALL_DIR
-mkdir -p $INSTALL_DIR
-
-rm -rf $INSTALL_DIR/lib $DIR/include
+rm -rf $INSTALL_DIR/lib $INSTALL_DIR/include $DIR/include
 mkdir -p $INSTALL_DIR/lib $INSTALL_DIR/include $DIR/include
-cp -r $DIR/maplibre/build/src/core/*.so* $INSTALL_DIR/lib
+cp -Prv $DIR/maplibre/build/src/core/*.so* $INSTALL_DIR/lib
 cp -r $DIR/maplibre/build/src/core/include/* $INSTALL_DIR/include
 cp -r $DIR/maplibre/src/**/*.hpp $DIR/include
