maxime-desroches/tinypilot

Added Qualcomm horizontal accuracy

system/qcomgpsd/qcomgpsd.py
@@ -348,6 +348,7 @@ def main() -> NoReturn:
       gps.unixTimestampMillis = dt_timestamp.timestamp()*1e3
       gps.source = log.GpsLocationData.SensorSource.qcomdiag
       gps.vNED = vNED
+      gps.accuracy = report["q_FltHdop"]
       gps.verticalAccuracy = report["q_FltVdop"]
       gps.bearingAccuracyDeg = report["q_FltHeadingUncRad"] * 180/math.pi if (report["q_FltHeadingUncRad"] != 0) else 180
       gps.speedAccuracy = math.sqrt(sum([x**2 for x in vNEDsigma]))

andiradulescu/openpilot

system/qcomgpsd/qcomgpsd.py
@@ -348,6 +348,7 @@ def main() -> NoReturn:
       gps.unixTimestampMillis = dt_timestamp.timestamp()*1e3
       gps.source = log.GpsLocationData.SensorSource.qcomdiag
       gps.vNED = vNED
+      gps.horizontalAccuracy = report["q_FltHdop"]
       gps.verticalAccuracy = report["q_FltVdop"]
       gps.bearingAccuracyDeg = report["q_FltHeadingUncRad"] * 180/math.pi if (report["q_FltHeadingUncRad"] != 0) else 180
       gps.speedAccuracy = math.sqrt(sum([x**2 for x in vNEDsigma]))