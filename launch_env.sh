3c7ea6adf912a00decc02904c9cc303477ad77fa
Andrei Radulescu <andi.radulescu@gmail.com>
2024-08-14 14:43:39 +0300

    agnos version commit hash test

--- launch_env.sh
+++ launch_env.sh
@@ -7,7 +7,7 @@ export OPENBLAS_NUM_THREADS=1
 export VECLIB_MAXIMUM_THREADS=1
 
 if [ -z "$AGNOS_VERSION" ]; then
-  export AGNOS_VERSION="10.1"
+  export AGNOS_VERSION="30a7fcb89e9e5f8398c16ced7ae08626f68a3e91"
 fi
 
 export STAGING_ROOT="/data/safe_staging"
