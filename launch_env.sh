b1588ad134dfb6d478f4cc05abbd376616f67a2d
Andrei Radulescu <andi.radulescu@gmail.com>
2024-08-19 22:46:29 +0300

    AGNOS 11 5G image

--- launch_env.sh
+++ launch_env.sh
@@ -7,7 +7,7 @@ export OPENBLAS_NUM_THREADS=1
 export VECLIB_MAXIMUM_THREADS=1
 
 if [ -z "$AGNOS_VERSION" ]; then
-  export AGNOS_VERSION="10.1"
+  export AGNOS_VERSION="25e29f7f3cdf2db46f23f5bf176a09412f0ca7d3"
 fi
 
 export STAGING_ROOT="/data/safe_staging"
