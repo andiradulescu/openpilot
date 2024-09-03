e5dcb7f43d93d3ac15534f2b60c01c060d1cd211
Andrei Radulescu <andi.radulescu@gmail.com>
2024-09-03 19:26:17 +0300

    agnos - pyenvtouv branch

--- launch_env.sh
+++ launch_env.sh
@@ -7,7 +7,7 @@ export OPENBLAS_NUM_THREADS=1
 export VECLIB_MAXIMUM_THREADS=1
 
 if [ -z "$AGNOS_VERSION" ]; then
-  export AGNOS_VERSION="10.1"
+  export AGNOS_VERSION="4049ecee55542c71e4c00b7c60a80162a0e41eed"
 fi
 
 export STAGING_ROOT="/data/safe_staging"
